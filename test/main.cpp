cf58bf98036f2ff7f54cbfc7be71c6dc56d7cce5
magnus <Magnusbackyard@live.com>
2016-04-01 23:00:30 +0800

    echo raw literal

--- test/main.cpp
+++ test/main.cpp
@@ -9,6 +9,8 @@
 #include <functional>
 #include "../declfn.h"
 
+#define ECHO(message) _Pragma(R"(message("message"))")
+
 // function
 static bool raw_returned_fn() { return true; }
 static void raw_int_arg_fn(int) {}
@@ -23,39 +25,40 @@ namespace Functor {
 
 class NormalClass {};
 
+
 int main(int argc, const char * argv[]) {    
-#warning "it should be cast to `std::function<bool()>` when given raw_returned_fn"
+ECHO("it should be cast to `std::function<bool()>` when given raw_returned_fn")
   static_assert(std::is_same<std::function<bool()>, declfn(raw_returned_fn)>::value, "!");
-#warning "it should be cast to `std::function<void(int)>` when given raw_int_arg_fn"
+ECHO("it should be cast to `std::function<void(int)>` when given raw_int_arg_fn")
   static_assert(std::is_same<std::function<void(int)>, declfn(raw_int_arg_fn)>::value, "!");
-#warning "it should be cast to `std::function<void(int&)>` when given raw_ref_arg_fn"
+ECHO("it should be cast to `std::function<void(int&)>` when given raw_ref_arg_fn")
   static_assert(std::is_same<std::function<void(int&)>, declfn(raw_ref_arg_fn)>::value, "!");
 
-#warning "it should be cast to `std::function<bool()>` when given Functor::returned instance"
+ECHO("it should be cast to `std::function<bool()>` when given Functor::returned instance")
   {
     Functor::returned functor;
     static_assert(std::is_same<std::function<bool()>, declfn(functor)>::value, "!");
   }
 
-#warning "it should be cast to `std::function<bool()>` when given returned lambda expression"
+ECHO("it should be cast to `std::function<bool()>` when given returned lambda expression")
   {
     auto lambda = [] { return true; };
     static_assert(std::is_same<std::function<bool()>, declfn(lambda)>::value, "!");
   }
 
-#warning "it should be cast to `std::function<void(int)>` when given returned lambda expression"
+ECHO("it should be cast to `std::function<void(int)>` when given returned lambda expression")
   {
     auto lambda = [](int) {};
     static_assert(std::is_same<std::function<void(int)>, declfn(lambda)>::value, "!");
   }
 
-#warning "it should be same to `std::function<bool(void)>` when given `std::function<bool(void)>` instance"
+ECHO("it should be same to `std::function<bool(void)>` when given `std::function<bool(void)>` instance")
   {
     std::function<bool(void)> fn;
     static_assert(std::is_same<std::function<bool(void)>, declfn(fn)>::value, "!");
   }
 
-#warning "it should be false type when given non-callable"
+ECHO("it should be false type when given non-callable")
   {
     NormalClass instance;
     static_assert(std::is_same<std::false_type, declfn(instance)>::value, "!");
